b25f41d1b30e72f221418e46842129bb5ae15cf1
Mark Brown <markbrown4@gmail.com>
2018-02-03 12:57:27 +0000

    starting point

--- src/server.js
+++ src/server.js
@@ -2,12 +2,6 @@
 const path = require('path')
 const express = require('express')
 const layout = require('express-layout')
-const bodyParser = require('body-parser')
-const validator = require('express-validator')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const flash = require('express-flash')
-const csrf = require('csurf')
 
 const routes = require('./routes')
 const app = express()
@@ -17,13 +11,7 @@ app.set('view engine', 'ejs')
 
 const middlewares = [
   layout(),
-  express.static(path.join(__dirname, 'public')),
-  bodyParser.urlencoded(),
-  cookieParser('super-secret-key'),
-  session({ cookie: { maxAge: 60000 } }),
-  csrf({ cookie: true }),
-  validator({ extended: false }),
-  flash()
+  express.static(path.join(__dirname, 'public'))
 ]
 app.use(middlewares)
 
