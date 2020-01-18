9a3fefc43e060dd5ef2d054a1e292e70c6a2477a
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2020-01-18 18:01:14 +0100

    Added starter files

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
-
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
-module.exports = router
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,31 +1,30 @@
+const path = require('path');
+const express = require('express');
+const layout = require('express-layout');
 
-const path = require('path')
-const express = require('express')
-const layout = require('express-layout')
+const routes = require('./routes');
+const app = express();
 
-const routes = require('./routes')
-const app = express()
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 const middlewares = [
   layout(),
-  express.static(path.join(__dirname, 'public'))
-]
-app.use(middlewares)
+  express.static(path.join(__dirname, 'public')),
+];
+app.use(middlewares);
 
-app.use('/', routes)
+app.use('/', routes);
 
 app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!")
-})
+  res.status(404).send("Sorry can't find that!");
+});
 
 app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(3000, () => {
-  console.log(`App running at http://localhost:3000`)
-})
+  console.log('App running at http://localhost:3000');
+});
