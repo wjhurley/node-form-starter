335765a1c2d1a6fcb2f3570c4077de9c449aee30
wjhurley <wjhurley@comcast.net>
2020-02-15 20:45:02 -0800

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,46 @@
 const express = require('express');
+const { check, validationResult, matchedData } = require('express-validator');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', {
+    data: {},
+    errors: {},
+  });
+});
+
+router.post('/contact', [
+  check('message')
+    .isLength({ min: 1 })
+    .withMessage('Message is required')
+    .trim(),
+  check('email')
+    .isEmail()
+    .withMessage('That email doesn‘t look right')
+    .bail()
+    .trim()
+    .normalizeEmail(),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render('contact', {
+      data: req.body,
+      errors: errors.mapped(),
+    });
+  }
+
+  
+
+  const data = matchedData(req);
+  console.log('Sanitized:', data);
+
+  req.flash('success', `Thanks for the message! Here's what you provided: ${JSON.stringify(data)}`);
+  res.redirect('/');
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const express = require('express');
 const layout = require('express-layout');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('express-flash');
 
 const routes = require('./routes');
 const app = express();
@@ -11,6 +15,16 @@ app.set('view engine', 'ejs');
 const middlewares = [
   layout(),
   express.static(path.join(__dirname, 'public')),
+  bodyParser.urlencoded({ extended: true }),
+  cookieParser(),
+  session({
+    secret: 'super-secret-key',
+    key: 'super-secret-cookie',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 },
+  }),
+  flash(),
 ];
 app.use(middlewares);
 
